eabf1a101fcce575c11e3a4bde2d44a5949d564c
Alexander Sauro <112291051+alxbro1@users.noreply.github.com>
2024-09-07 15:42:19 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,11 @@
 import app from "./server";
-import { PORT } from "./config/envs";
 import { AppDataSource } from "./config/data-source";
 import CreateUsers from "./helpers/CreateUsers";
 import CreateAppoinments from "./helpers/CreateAppoinments";
 
 AppDataSource.initialize().then(() => {
-  app.listen(PORT, () => {
-    console.log(`server is listening on port ${PORT}`);
+  app.listen(3000, () => {
+    console.log(`server is listening on port ${3000}`);
   });
   CreateUsers().then(() => {
     CreateAppoinments();
